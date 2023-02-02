04d2141911be328050de079c74f17fa4875f34d1
kevin <kwangje1@gmail.com>
2023-02-02 21:52:54 +0900

    chore: update configs.

--- apps/dashboard-solid/tailwind.config.cjs
+++ apps/dashboard-solid/tailwind.config.cjs
@@ -1,10 +1,2 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = require("tailwind-config/tailwind.config.cjs");
-
-// module.exports = {
-//   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {}
-//   },
-//   plugins: []
-// };
